20c5d2eee4b7f28249870604f42c90a9dc34ec0b
ajasn <ajas.neduvanchery@gmail.com>
2025-04-16 17:21:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,9 @@ st.dataframe(pd_df)
 #st.dataframe(data=my_dataframe,use_container_width=True)
 #st.stop()
 
-   sr = requests.get("https://my.smoothiefroot.com/api/fruit/all")
+sr = requests.get("https://my.smoothiefroot.com/api/fruit/all")
 #st.text(smoothiefroot_response.json())
-        sf_df1 = st.dataframe(data=sr.json(),use_container_width=True)
+sf_df1 = st.dataframe(data=sr.json(),use_container_width=True)
 st.stop()
 
 ingredient_list = st.multiselect ("Choose up to 5 Incredients", my_dataframe, max_selections=5)
